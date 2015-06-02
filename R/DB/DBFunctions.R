1aa79a875927653a3fa15585861056f4befcf44e
erik verschueren <erik.verschueren@gmail.com>
2015-06-01 17:29:16 -0700

    added function to get table names from DB (handy to use when tablenames need to be defined in interface functions)

--- R/DB/DBFunctions.R
+++ R/DB/DBFunctions.R
@@ -7,6 +7,13 @@ myConnect = function(dbname='FluOMICS'){
   return(con)
 }
 
+getTableNames = function(){
+  con = myConnect()
+  res = dbGetQuery(con, 'show tables;')
+  dbDisconnect(con)
+  return(res)
+}
+
 getGenomics = function(cell_line, strain, lfc, q, table, mapping='mygene_human_1to1', condition_1='.*', condition_2='.*'){
   con = myConnect()
   query = sprintf("select E.experiment_id, E.omics_type, E.condition_1, E.condition_2, E.cell_line, E.strain, E.status, '' as 'kegg_id', GP.uniprot_ac, R.entrez_id, R.log2fc, R.q_value from experiments E join `%s` R on R.`experiment_id` = E.`experiment_id` and R.`sample_1` = E.`sample_1_code` and R.`sample_2` = E.`sample_2_code` left join %s GP on GP.`entrez_id` = R.`entrez_id` where E.`cell_line` regexp '%s' and E.`strain` regexp '%s' and R.`q_value` < %s and abs(R.`log2fc`) > %s and E.`condition_1` regexp '%s' and E.`condition_2` regexp '%s'", table, mapping, cell_line, strain, q, lfc, condition_1, condition_2)
